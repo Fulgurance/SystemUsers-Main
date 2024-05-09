51ceb16bc512a2c1ebbde027df4d926bfd44acbc
Fulgurance <zohran.londais@gmail.com>
2024-05-09 09:46:53 +0100

    Added new user

---
+++ Lp/0.1.0/0.1.0.cr
@@ -0,0 +1,15 @@
+class Target < ISM::VirtualSoftware
+
+    def install
+        super
+
+        runUserAddCommand(["-c","\"Print Service User\"","-d","/var/spool/cups","-g","lp","-s","/bin/false","-u","9","lp"])
+    end
+
+    def uninstall
+        super
+
+        runUserDelCommand(["-frZ","lp"])
+    end
+
+end
