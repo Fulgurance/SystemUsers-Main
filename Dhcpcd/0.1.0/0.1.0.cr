fa505f0b5871e814f40b3d272cca05b4e9ad2ca2
Fulgurance <zohran.londais@gmail.com>
2024-08-17 12:46:48 +0100

    Updated to last standard

--- Dhcpcd/0.1.0/0.1.0.cr
+++ Dhcpcd/0.1.0/0.1.0.cr
@@ -3,11 +3,11 @@ class Target < ISM::VirtualSoftware
     def install
         super
 
-        runUserAddCommand([ "-c \'dhcpcd PrivSep\'",
-                            "-d /var/lib/dhcpcd",
-                            "-g dhcpcd",
-                            "-s /bin/false",
-                            "-u 52 dhcpcd")
+        runUserAddCommand(  "-c \'dhcpcd PrivSep\'  \
+                            -d /var/lib/dhcpcd      \
+                            -g dhcpcd               \
+                            -s /bin/false           \
+                            -u 52 dhcpcd")
     end
 
     def uninstall
