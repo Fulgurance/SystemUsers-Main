336f20b8b393229ed81fc33b6eacc6a2f4875465
Fulgurance <zohran.londais@gmail.com>
2024-12-13 15:54:57 +0000

    Updated wrong id

--- Ism/0.1.0/0.1.0.cr
+++ Ism/0.1.0/0.1.0.cr
@@ -3,7 +3,7 @@ class Target < ISM::VirtualSoftware
     def install
         super
 
-        runUserAddCommand("-c \"Special user for system management\" -d /var/lib/ism/home -u255 -g255 -s /bin/bash ism")
+        runUserAddCommand("-c \"Special user for system management\" -d /var/lib/ism/home -u250 -g250 -s /bin/bash ism")
     end
 
     def uninstall
