9861fb806c50dbbef505ca0978a74ede5c452350
Fulgurance <zohran.londais@gmail.com>
2025-02-22 17:55:48 +0000

    Conforming to new ism standard

--- Ism/0.1.0/0.1.0.cr
+++ Ism/0.1.0/0.1.0.cr
@@ -3,7 +3,7 @@ class Target < ISM::VirtualSoftware
     def install
         super
 
-        runUserAddCommand("-c \"Special user for system management\" -d /var/lib/ism/home -u250 -g250 -s /bin/bash ism")
+        runUserAddCommand("-c \"Special user for system management\" -d /var/lib/ism/home -u#{Ism.systemId} -g#{Ism.systemId} -s /bin/bash ism")
     end
 
     def uninstall
